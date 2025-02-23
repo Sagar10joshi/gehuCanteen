63e9e0bfaca7e92c779434b43c800436a132a2d5
Sagar Joshi <coderjoshi15@gmail.com>
2025-02-23 12:59:01 +0530

    linkchange  commit

--- canteen/src/Ownerchat.jsx
+++ canteen/src/Ownerchat.jsx
@@ -180,7 +180,7 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("https://gehu-canteen-5ni8-sagars-projects-0f20619e.vercel.app", { transports: ["websocket"],withCredentials: true });
+const socket = io("https://gehu-canteen-5ni8.vercel.app", { transports: ["websocket"],withCredentials: true });
 
 const OwnerChat = () => {
   const [messages, setMessages] = useState([]);
